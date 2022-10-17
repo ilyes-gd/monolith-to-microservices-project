19b3259f7bb4c7f707334e279f68c297b7497fcf
ilyesgdouda <gdoudailyes@gmail.com>
2022-10-17 01:38:35 +0100

    add kubernetes object and new version of front end

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab0aae100047b439d9ea1635f2fb8a53-1962617785.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
